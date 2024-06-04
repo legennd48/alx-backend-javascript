7c51176190e9bbc548b78b9764a32ef49cee5e2b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-05 00:53:58 +0100

    simplified code and removed sigint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,16 +4,12 @@
  * @author Abdulrazzaq Liasu
  */
 
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
-
-process.on('SIGINT', () => {
+process.stdin.on('readable', () => {
+  const name = process.stdin.read()
+  process.stdout.write(`Your name is: ${name}`);
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });
