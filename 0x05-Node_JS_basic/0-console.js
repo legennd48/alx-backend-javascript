e9f9755a85a3810c8a98785c325f8bc47011dd16
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-05 00:19:15 +0100

    initial commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints given string argument to stdout
+ * @param {string} text
+ * @author Abdulrazzaq Liasu
+ */
+const displayMessage = (text) => {
+    console.log(`${text}`);
+};
+
+module.exports = displayMessage;
