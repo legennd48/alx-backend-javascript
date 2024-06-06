0af7ac7b252ff0aa71ac02577347c7cc517e5faa
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-06 22:34:28 +0100

    initial commit

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should track calculateNumber function', () => {
+    const spy = sinon.spy(console);
+    const stub = sinon.stub(Utils, 'calculateNumber');
+
+    stub.returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.callCount).to.be.equal(1);
+    expect(spy.log.calledWith('The total is: 10')).to.be.true;
+    expect(spy.log.callCount).to.be.equal(1);
+    stub.restore();
+    spy.log.restore();
+  });
+});
