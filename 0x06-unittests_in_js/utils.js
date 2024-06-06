2dfb1265b19e551f78e60a17769b65d4ce0e6252
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-06 22:14:45 +0100

    initial commit

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    // Round both numbers using Math.round()
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    if (type === 'SUM') {
+      return roundedA + roundedB;
+    }
+    if (type === 'SUBTRACT') {
+      return roundedA - roundedB;
+    }
+    if (type === 'DIVIDE') {
+      return roundedB === 0 ? 'Error' : roundedA / roundedB;
+    }
+    return 0;
+  },
+};
+
+module.exports = Utils;
