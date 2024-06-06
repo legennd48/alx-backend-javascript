553493ea691f7de19524e072aa7292e5eb701516
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-07 00:06:26 +0100

    initial commit

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+// Listen on port 7865
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+// Route for GET /
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
+});
+
+module.exports = app;
